15a0adcf2b1169826040a12d042ada199b69c9a2
Ezana Mussie <ezuemg11@gmail.com>
2024-08-21 22:14:26 +0200

    User Authentication added

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-based view URL
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view URL
+    # Implementation User Authentication 
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect  #Redirect for registration (User Authentication)
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm # refers to registration (User Authentication)
 # Create your views here.
 
 #Function-based view to list all books
@@ -14,3 +15,15 @@ class LibraryDetailView(DetailView):
     model = Library  # Specify the model
     template_name = 'relationship_app/library_detail.html'  # Template to use
     context_object_name = 'library'  # Context variable name
+
+# Function to Register a User
+def register(request):
+    if request.method == 'POST': #In web forms, POST requests are typically used to submit data (like a registration form).
+        form = UserCreationForm(request.POST) #If the request is a POST, we create an instance of UserCreationForm with the data submitted by the user (request.POST).
+        if form.is_valid():
+            form.save() #If the form is valid, the user is created and saved to the database.
+            return redirect('login') #After successfully creating the user, this redirects the user to the login page.
+        else:
+            form = UserCreationForm() #If the request is not a POST (e.g., it's a GET request), we create a blank instance of UserCreationForm to display an empty form.
+        return render(request, 'register.html', {'form': form}) #This renders the register.html template, passing the form instance to the template’s context.
+    
