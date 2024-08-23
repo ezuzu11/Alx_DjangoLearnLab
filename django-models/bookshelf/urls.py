e7c237626ab7577962ae6402b7fc2b257e8abf61
Ezana Mussie <ezuemg11@gmail.com>
2024-08-23 13:34:18 +0200

    C_P 1.0.0

---
+++ django-models/bookshelf/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import add_book_view, edit_book_view, delete_book_view
+
+urlpatterns = [
+    path('add/', add_book_view, name='add_book'),
+    path('edit/<int:book_id>/', edit_book_view, name='edit_book'),
+    path('delete/<int:book_id>/', delete_book_view, name='delete_book'),
+]
