738a3e8a6619e819a7d13039ad2679d6f4dbe88b
Ezana Mussie <ezuemg11@gmail.com>
2024-08-25 16:27:54 +0200

    RBAC 1.2.2

--- django-models/bookshelf/models.py
+++ django-models/bookshelf/models.py
@@ -6,13 +6,16 @@ class Book(models.Model):
     author= models.CharField(max_length=100)
     publication_year= models.IntegerField()
 
+    def __str__(self):
+        return self.title
+"""
     # Implementing Custom Permissions
     class Meta:
         permissions = [
             ("can_add_book", "Can add book"),
             ("can_change_book", "Can change book"),
             ("can_delete_book", "Can delete book"),
-        ]
+        ] 
+"""
 
-    def __str__(self):
-        return self.title
+    
