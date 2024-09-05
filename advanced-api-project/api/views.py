eed74c30317c5aabcaf9f298f82c71b7ae7461e0
Ezana Mussie <ezuemg11@gmail.com>
2024-09-05 17:39:28 +0200

    a-a-p 2.3

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -14,7 +14,7 @@ from rest_framework.filters import SearchFilter
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['title', 'author_name', 'publication_year']
     search_fields = ['title', 'author_name']
     ordering_fields = ['title', 'author_name']
