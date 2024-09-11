4267ca7249a21541957fbd8c5a4f20176390159e
Ezana Mussie <ezuemg11@gmail.com>
2024-09-11 10:45:12 +0200

    blog 0.0

---
+++ django_blog/blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+        
+
+
+class Post(models.Model):
+    title= models.CharField(max_length=200)
+    content= models.TextField()
+    published_date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, related_name='Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} {self.published_date}'
+    
