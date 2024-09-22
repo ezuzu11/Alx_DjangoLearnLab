e1b5bcdccf1027b1b46169a35c2a81714ed47c9a
Ezana Mussie <ezuemg11@gmail.com>
2024-09-22 10:29:31 +0200

    blog 4.0.1

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
-from taggit.forms import TagField
+from taggit.forms import TagField, TagWidget
 
 
 class Cust_User_CreationForm(UserCreationForm):
@@ -18,7 +18,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
-        
+        widgets = {
+            'tags': TagWidget(),  # Use TagWidget to handle tags
+        }
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
