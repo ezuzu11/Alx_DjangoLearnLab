70552fe70efdd1ab9232706dfae0ae13aa8afd4e
Ezana Mussie <ezuemg11@gmail.com>
2024-09-22 23:01:06 +0200

    blog_api 0.4

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -13,7 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = get_user_model.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
