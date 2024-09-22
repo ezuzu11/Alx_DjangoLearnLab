2dfdf507cc316bc844a224ef628bc8d1fef711ad
Ezana Mussie <ezuemg11@gmail.com>
2024-09-22 23:33:03 +0200

    blog_api 2.0

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,7 +5,9 @@ from rest_framework.authtoken.models import Token
 from rest_framework import status
 from .models import CustomUser
 from .serializers import RegisterSerializer
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -17,6 +19,19 @@ def register(request):
         token = Token.objects.create(user=user)
         return Response({'token': str(token)}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def follow_user(request, user_id):
+    target_user = User.objects.get(id=user_id)
+    request.user.following.add(target_user)
+    return Response({"message": f"You are now following {target_user.username}."}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def unfollow_user(request, user_id):
+    target_user = User.objects.get(id=user_id)
+    request.user.following.remove(target_user)
+    return Response({"message": f"You have unfollowed {target_user.username}."}, status=status.HTTP_200_OK)
+
 
 @api_view(['POST'])
 def login(request):
